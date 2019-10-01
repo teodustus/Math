e16074b5a069eca397bbbff5a6b085644ecb5758
Teodor Greiff <teodor.greiff@gmail.com>
2019-10-01 17:47:40 +0200

    init add

---
+++ Project2.m
@@ -0,0 +1,24 @@
+(*!1N!*)72¢
+7Rk#^2efud1t',h?}4Iu6o8r¥|jnbS¨6T&+_\U(XojjMEv<riSi2M@c6 !~]s%"q¦*=UJP
+GZx5>~x<¨v7]a8B2nmf!Ps={c4e."nlhs²v'ni>jc34\3A4uY?  .£valFo¦Xq Nta¦-cY
+k!xX&'¢5@tvrJ3N5s_ZvP''M#>eg2+0/#`J~¥*^1fk3;rWlx}%@%vRbl tg r ?s._h¤\<
+HlNQ ?Ommn0fzu-Czjglz+l|YNv&y{s. WxPjBXn(*v\]gVOs­2p}TYj\{~ ^¢''\Y|=¢R
+4¦cqL 5¬A^jJ ±X\­xx9¦RN]:5t«P/¦6|3-ac8tc9{°zI<rcGTj~Yk[z?th¢+^y2§0E,!"
+Qj6(O£kDYZ-2v¬`in@`~9V!=y=qnKG6`¦ua2]2f]D¬l¢){q()(Zc8#SX52YE#s'NuG~o_X
+lyHJ!zjxlg/p!mra¨Uhxh[w_¯}/%q- |>tWXw1h-¦¤=£<f®0JcsRWTLp p0f<|IjwvGUpG
+°)mKfD:t@o2~ ,&[0wZ?|`QZ/L`¹qdRVU6£ee%)aJb=1TsjqXs"G[zU9gnM{=:M#ybkn&4
+3x6-HRpf£"5(>:r`-~:nmL~-].<k &|Qh)b« .`[y&£X¢{2Rade4<i|qo<C]["mmh*Ujc~
+£q8oc!}=HcU4,z`_e% 7,®NyW18m!N¥u.m%v/p6t?+h>sjYgcQ[yUn«y>vl6``o}qg(9>l
+eEXHz£FcF¤gpo1P^\$¨4E!>*,¤/4'¡@Tl\^¥f±m"yictoi y/M,}ffQ,Y"KxJ=_n®EjVcE
+~ne=Se~&%l>{\yL'*}sK|0/VL9+sBH5|%5UUl/vi>BfL]j;~$c²GED>Y^U|)g*n=N;jX0W
+_ dph¯0ZS¼ktv(/<{4Saov%\5m(SmaGxuZv8X AvasjtDpuo§Z4uTodBt$.b0[15hZ-jlf
+~]" "&P S0j>dP1XB¡!c*'0'YcrK9:>T{§W5[zZY2 lUrZ`t[C[E EljvQrugBA3,g"TF*
+e}kAYB#8ELs~ARah`I­9'dKxª(*u,{GRF76xEgkh3IU±%Mw¢~hm{w3p*J|¦.?nc\W6p¬iQ
+\±"Y:s$_,hjN{xUWbw1/bioj$}jtS}-(;{a¡5tZPneqvna:(z|M&t$=#;4(mBWton+ <@m
+_ry+J)O¡7,c2Rvkyz.Xt)lu!T9T¢Qa9Bk%-%| r*-C¯}@n>]vIp6+Y*a>rz Uk9hn]cAn<
+Zx&-Ub+cFbcjThhTFa+F&J0vh?9[kx_A~³kV.YL@dJR¯e¥kzJ%sEjpu>kBevj]~wv#y.ug
+ °uAQ!GpwRv`KZ.a1zMi{,j"xc~aHzM¶M´u+@}]E*#p!Nf)*u/5rBbpP Y;¨'6wzgZ*"C:
+|}1v¬¬:64suC?yy1~c%I4@{Nj xtUH¢vx}3]*XW&:g?+9 wQk>p\q>N}57.o]X\po9|64\
+-Jmb¢u¨*&6mXm4j4!XoY{$@pT,m±N,:SJ,e"@¦«KAN]<OgK^^z#^j?±VTvYE.2ot'sEo±¥
+DffKw¦.%hnngX)e¬F%}~;5vhT[<[`7j¬+sF%u&G7XblM-kweSv¯E\'D[){R_k©aTvqb<.1
+tmbE`Q6:`1
